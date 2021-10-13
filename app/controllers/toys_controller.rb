fc704fde958a5efb6ff36107f2ee683200670db0
John Sangalang <sangalangjl@gmail.com>
2021-10-12 22:47:55 -0400

    commit tree bdc66f702723e2a6376fd977bf91fd411fe4d17d

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,19 +7,34 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    if toy 
+      toy.update(toy_params)
+      render json: toy
+    else
+      render json: { error: "Toy not found" }, status: :not_found
+    end
   end
 
   def destroy
     toy = Toy.find_by(id: params[:id])
     toy.destroy
     head :no_content
+  end
+
+  def increment_likes
+    toy = Toy.find_by(id: params[:id])
+    if toy 
+      toy.update(likes: toy.likes + 1)
+      render json: toy
+    else
+      render json: { error: "Toy not found" }, status: :not_found
+    end
   end
 
   private
